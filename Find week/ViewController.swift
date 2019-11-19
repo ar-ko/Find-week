1e3d7532d557d5871a2c66d4ff6a0171353f8e59
ar_ko <ar_ko>
2019-11-19 19:50:01 +0300

    Готов основной функционал Осталось поработать над обработкой ошибок и вводом стартовой даты

--- Find week/ViewController.swift
+++ Find week/ViewController.swift
@@ -9,12 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var resultLabel: UILabel!
+       
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        resultLabel.text = findWeek()
     }
-
-
+        
+    
+    @IBAction func rollDatePicker(_ sender: UIDatePicker) {
+        resultLabel.text = findWeek(sender.date)
+  
+    }
+    
+    // MARK: Установка даты начала отсчета
+    func getStartDate() -> Date {
+        let calendar = Calendar.current
+        var dateComponents = DateComponents()
+        
+        dateComponents.day = 2
+        dateComponents.month = 9
+        dateComponents.year = 2019
+        let date = calendar.date(from: dateComponents)
+        
+        return date!
+    }
+    
+    func findWeek(_ date:Date = Date()) -> String {
+        var result:String = ""
+        
+        let diffInDays = Calendar.current.dateComponents([.day], from: getStartDate(), to: date).day
+        resultLabel.text = String(diffInDays!)
+        
+        let numberOfWeek = diffInDays! / 7
+        
+        switch numberOfWeek % 2 {
+        case 0:
+            result = "White week"
+        default:
+            result = "Blue week"
+        }
+        
+        return result
+    }
 }
 
