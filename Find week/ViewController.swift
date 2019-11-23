d6801d22e47812966c868980d4fbc7b7d5de368f
ar_ko <ar_ko>
2019-11-23 17:00:35 +0300

    ver 0.1 Теперь корректно определяет неделю в независимости от дня начала обучения Обработка ошибок

--- Find week/ViewController.swift
+++ Find week/ViewController.swift
@@ -9,50 +9,73 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet var resultLabel: UILabel!
-       
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var startDateButton: UIButton!
     
+   
     override func viewDidLoad() {
+
         super.viewDidLoad()
-        
+        datePicker.minimumDate = getStartDate()
         resultLabel.text = findWeek()
     }
-        
+    
     
     @IBAction func rollDatePicker(_ sender: UIDatePicker) {
+       
         resultLabel.text = findWeek(sender.date)
-  
     }
     
     // MARK: Установка даты начала отсчета
-    func getStartDate() -> Date {
+    func getStartDate() -> Date? {
+        
         let calendar = Calendar.current
         var dateComponents = DateComponents()
         
         dateComponents.day = 2
         dateComponents.month = 9
         dateComponents.year = 2019
-        let date = calendar.date(from: dateComponents)
         
-        return date!
-    }
-    
-    func findWeek(_ date:Date = Date()) -> String {
-        var result:String = ""
-        
-        let diffInDays = Calendar.current.dateComponents([.day], from: getStartDate(), to: date).day
-        resultLabel.text = String(diffInDays!)
-        
-        let numberOfWeek = diffInDays! / 7
-        
-        switch numberOfWeek % 2 {
-        case 0:
-            result = "White week"
-        default:
-            result = "Blue week"
+        guard let date = calendar.date(from: dateComponents) else {
+            return nil
         }
         
-        return result
+        return date
     }
+    
+    // MARK: Узнаем четная неделя или нет
+    func findWeek(_ date:Date = Date()) -> String {
+        
+        let calendar = Calendar.current
+        var result = String()
+        
+        guard let startDate = getStartDate() else {
+            return "StartDate is incorrect"
+        }
+        
+        let diffInDays = Calendar.current.dateComponents([.day], from: startDate, to: date).day
+            
+        guard let days: Int = diffInDays else {
+            return "diffInDays is incorrect"
+        }
+        
+        // Коррекция даты, находим начало недели
+        var dateCorrection = calendar.component(.weekday, from: startDate) - 2
+        if calendar.component(.weekday, from: startDate) == 1 {
+            dateCorrection += 7
+        }
+        
+        
+        let numberOfWeek = (days + dateCorrection) / 7
+        switch numberOfWeek.isMultiple(of: 2){
+                 case true:
+                     result = "White week"
+                 case false:
+                     result = "Blue week"
+                 }
+                 return result
+            }
+    
 }
 
