87929ad9d54f529b8061bda27f2e4805844404f2
ar_ko <arTEAMko@icloud.com>
2019-12-01 22:02:47 +0300

    ver 0.2 Появилась возможность выбрать дату начала отсчета Небольшие изменения в дизайне

--- Find week/ViewController.swift
+++ Find week/ViewController.swift
@@ -11,50 +11,72 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var resultLabel: UILabel!
     @IBOutlet weak var datePicker: UIDatePicker!
-    @IBOutlet weak var startDateButton: UIButton!
+    @IBOutlet weak var startDateField: UITextField!
     
-   
+    let startDatePicker = UIDatePicker()
+    
+    // MARK: Установка даты начала отсчета
+     func setStartDate() -> Date? {
+         
+         let calendar = Calendar.current
+         var dateComponents = DateComponents()
+         
+         dateComponents.day = 2
+         dateComponents.month = 9
+         dateComponents.year = 2019
+         
+         guard let date = calendar.date(from: dateComponents) else {
+             return nil
+         }
+         
+         return date
+     }
+    
+    var startDate = Date()
+    
     override func viewDidLoad() {
 
         super.viewDidLoad()
-        datePicker.minimumDate = getStartDate()
-        resultLabel.text = findWeek()
+        startDate = setStartDate()!
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd MMMM yyyy"
+        let dateS = formatter.string(from: startDate)
+        startDateField.text = "Start date is \(dateS)"
+        
+        datePicker.minimumDate = startDate
+        resultLabel.text = findWeek(Date(), startDate)
+        startDateField.inputView = startDatePicker
+        startDatePicker.datePickerMode = .date
+        startDatePicker.backgroundColor = resultLabel.textColor
+        
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneAction))
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        
+        toolbar.setItems([flexSpace, doneButton], animated: true)
+
+        
+        startDateField.inputAccessoryView = toolbar
+        
+        
     }
     
     
     @IBAction func rollDatePicker(_ sender: UIDatePicker) {
        
-        resultLabel.text = findWeek(sender.date)
+        resultLabel.text = findWeek(sender.date, startDate)
     }
     
-    // MARK: Установка даты начала отсчета
-    func getStartDate() -> Date? {
-        
-        let calendar = Calendar.current
-        var dateComponents = DateComponents()
-        
-        dateComponents.day = 2
-        dateComponents.month = 9
-        dateComponents.year = 2019
-        
-        guard let date = calendar.date(from: dateComponents) else {
-            return nil
-        }
-        
-        return date
-    }
+ 
     
     // MARK: Узнаем четная неделя или нет
-    func findWeek(_ date:Date = Date()) -> String {
+    func findWeek(_ currentDate:Date = Date(), _ startDate: Date) -> String {
         
         let calendar = Calendar.current
         var result = String()
-        
-        guard let startDate = getStartDate() else {
-            return "StartDate is incorrect"
-        }
-        
-        let diffInDays = Calendar.current.dateComponents([.day], from: startDate, to: date).day
+               
+        let diffInDays = Calendar.current.dateComponents([.day], from: startDate, to: currentDate).day
             
         guard let days: Int = diffInDays else {
             return "diffInDays is incorrect"
@@ -66,7 +88,6 @@ class ViewController: UIViewController {
             dateCorrection += 7
         }
         
-        
         let numberOfWeek = (days + dateCorrection) / 7
         switch numberOfWeek.isMultiple(of: 2){
                  case true:
@@ -77,5 +98,24 @@ class ViewController: UIViewController {
                  return result
             }
     
+    
+    @objc func doneAction() {
+        getDateFromStartDatePicker()
+        view.endEditing(true)
+    }
+    
+    func getDateFromStartDatePicker() {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd MMMM yyyy"
+        let dateS = formatter.string(from: startDatePicker.date)
+        
+        startDateField.text = "Start date is \(dateS)"
+        let date = formatter.date(from:dateS)!
+        startDate = date
+        datePicker.minimumDate = startDate
+        resultLabel.text = findWeek(datePicker.date, startDate)
+        
+        
+    }
 }
 
